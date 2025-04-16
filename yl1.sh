303d7f8234490afc56232ad75c97680b46867aa6
RobinRoosmaa <Robin.Roosmaa@VIKK.ee>
2025-04-16 15:29:53 +0300

    Ülessanne 1 viimane versioon.

---
+++ yl1.sh
@@ -0,0 +1,10 @@
+#!/bin/Bash
+#Skript tervitab ja ütleb kasutajale hetkel oleva kuupäeva kella ja aastaaja.
+#Autor: Robin Roosmaa
+#Viimati muudetud: 16.04.2025
+
+echo "Tere tulemast Bash skriptimise kursusele"
+echo
+echo "Täna on: $(date +%A" "%d/%m/%Y" "%T)"
+echo
+echo "See on hea päev Bash Skriptimise alustamiseks"
