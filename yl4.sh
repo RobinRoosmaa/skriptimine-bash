2791700c46c666a6c628dd1561ebf647c6cc7483
RobinRoosmaa <Robin.Roosmaa@VIKK.ee>
2025-04-23 16:04:53 +0300

    Lisatud commentaar.

--- yl4.sh
+++ yl4.sh
@@ -3,6 +3,7 @@
 #Autor: Robin Roosmaa
 #Viimati muudetud: 23.04.2025
 
+#Defineerib muutujad varundatava directori ja varunduspaiga ning varundab lisades aja ja kustutades kõik outputid.
 varundada=/var/log
 asukoht=/varundus
 tar -czvf $asukoht/logbu"_"$(date +%d-%m-%Y"_"%T).tar.gz $varundada &> /dev/null
